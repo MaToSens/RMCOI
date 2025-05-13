72ae1bed5845790f16dcf452e26d129397f04350
MaToSens <matoosens@gmail.com>
2025-05-13 11:07:16 +0200

    Added initial files

---
+++ RMCOI/RMCOIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMCOIApp.swift
+//  RMCOI
+//
+//  Created by MaTooSens on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RMCOIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
